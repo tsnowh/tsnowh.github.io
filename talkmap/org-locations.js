8030ad18ccf028dc6e3b3ebc3d83274a8db3f956
Thomas Snow <thomashs134@gmail.com>
2025-09-29 15:52:18 -0400

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
   [
-    "Online Algorithm for Fractional Matchings with Edge Arrivals in Graphs of Maximum Degree Three (WAOA 2025)<br />University of Warsaw, Faculty of Physics; Warsaw, Poland",
+    "Online Algorithm for Fractional Matchings with Edge Arrivals in Graphs of Maximum Degree Three<br />University of Warsaw, Faculty of Physics; Warsaw, Poland",
     20.982975,
     52.212728
   ]
